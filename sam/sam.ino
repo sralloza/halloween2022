cb1f754864fc575aaee244ec9389d399efa1c5c1
Diego Alloza González <sralloza@gmail.com>
2022-02-14 18:23:25 +0100

    chore: rename vars of sam

--- sam/sam.ino
+++ sam/sam.ino
@@ -1,27 +1,25 @@
-const int relay = 14;  // D5
-const int input = 12;  // D6
+const int relayPin = 14;
+const int inputPin = 12;
 const int delayms = 3000;
 
 void setup()
 {
     Serial.begin(9600);
     Serial.write("Starting...\n");
-    pinMode(relay, OUTPUT); //initialize the relay pin as an output
-    pinMode(input, INPUT);
-    digitalWrite(relay, HIGH);
+    pinMode(relayPin, OUTPUT);
+    pinMode(inputPin, INPUT);
+    digitalWrite(relayPin, HIGH);
     Serial.write("Setup ended\n");
 }
 
 void loop()
 {
-    // String msg = sprintf("data: %s", digitalRead(input));
-    // Serial.write("Hi there\n");
-    // delay(1000);
-    if (digitalRead(input) == HIGH){
+    if (digitalRead(inputPin) == HIGH)
+    {
         Serial.write("Starting loop\n");
-        digitalWrite(relay, LOW);
-        delay(delayms); //wait for 2s
-        digitalWrite(relay, HIGH);
+        digitalWrite(relayPin, LOW);
+        delay(delayms);
+        digitalWrite(relayPin, HIGH);
         Serial.write("Loop ended\n\n");
     }
 }
