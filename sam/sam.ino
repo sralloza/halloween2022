fef3307cb76035f8d42f7b951f4fa4daa889ca0a
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-03-18 14:15:59 +0100

    chore: change SAM algorithm to work with a switch

--- sam/sam.ino
+++ sam/sam.ino
@@ -1,26 +1,56 @@
 // Pins are setup for Arduino Nano
 const int relayPin = 10;
 const int inputPin = 12;
-const int delayms = 3000;
+const int delayms = 60000;
+
+const bool RELAY_ACTIVATION_HIGH = true;
+
+void setRelayON(int relayPin)
+{
+    if (RELAY_ACTIVATION_HIGH == true)
+    {
+        digitalWrite(relayPin, HIGH);
+    }
+    else
+    {
+        digitalWrite(relayPin, LOW);
+    }
+}
+
+void setRelayOFF(int relayPin)
+{
+    if (RELAY_ACTIVATION_HIGH == true)
+    {
+        digitalWrite(relayPin, LOW);
+    }
+    else
+    {
+        digitalWrite(relayPin, HIGH);
+    }
+}
+
 
 void setup()
 {
     Serial.begin(9600);
     Serial.write("Starting...\n");
     pinMode(relayPin, OUTPUT);
-    pinMode(inputPin, INPUT);
-    digitalWrite(relayPin, HIGH);
+    pinMode(inputPin, INPUT_PULLUP);
+    setRelayOFF(relayPin);
     Serial.write("Setup ended\n");
 }
 
 void loop()
 {
+    Serial.write(digitalRead(inputPin));
     if (digitalRead(inputPin) == HIGH)
     {
         Serial.write("Starting loop\n");
-        digitalWrite(relayPin, LOW);
+        setRelayON(relayPin);
         delay(delayms);
-        digitalWrite(relayPin, HIGH);
+        setRelayOFF(relayPin);
+        Serial.write("Stopping relay\n");
+        delay(delayms);
         Serial.write("Loop ended\n\n");
     }
 }
