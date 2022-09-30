29b642439639af94bab56997d4e8eddfcf51e252
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-09-30 21:04:15 +0200

    fix: no board found

--- deploy-nano.sh
+++ deploy-nano.sh
@@ -4,6 +4,11 @@ set -eo pipefail
 CIRCUIT=${1:?the first arg must be an arduino project}
 
 export PORT=$(arduino-cli board list | grep USB | awk '{print $1}')
+if [ -z "$PORT" ]; then
+  echo "No board found"
+  exit 1
+fi
+
 arduino-cli compile --fqbn arduino:avr:nano $1
 arduino-cli upload -p $PORT --fqbn arduino:avr:nano $1
 arduino-cli monitor -p $PORT
